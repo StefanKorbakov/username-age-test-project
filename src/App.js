be2ea219d271872237073390dbdfb5fbf0477d85
Stefan Korbakov <stefankorbakov@gmail.com>
2021-05-02 17:31:51 +0200

    Restarted from scratch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React, { useState } from 'react';
-import Username from './components/Username';
-import UsersList from './components/UsersList';
-
 
 function App() {
-  const [user, setUser] = useState('');
-
-  const addUserHandler = (userData) => {
-    setUser(userData);
-  }
-
   return (
     <div>
-      <Username onAddUser={addUserHandler}/>
-      <UsersList listOfUsers={user} />
+      
     </div>
   );
 }
